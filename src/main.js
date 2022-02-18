f5aaaa0a1352e3839afdfa66313aef7b963f82be
LauraCastellanos13 <lauracastellanosym@gmail.com>
2022-02-18 11:48:47 -0500

    Cambios header
    Co-authored-by: Angela Paola Huyo Millan <paolahuyo@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,22 +1,20 @@
-const $ghibli = document.querySelector('#imgGhibli')
-function renderGhibli(image) {
-    $ghibli.setAttribute('src', image)
-}
-fetch('data/ghibli/ghibli.json')
-    .then(function (res) {
 
-        return res.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        let html = '';
-        data.films.forEach(function (films) {
-            html += `
-    <li> FILM: ${films.title}, SCORE: ${films.rt_score}, DESCRIPTION: ${films.description}, DIRECTOR: ${films.director}, PRODUCER: ${films.producer}, RELEASE DATE: ${films.release_date} </li>
-    <img src="${films.poster}" alt="Poster's ${films.title}">
+fetch("data/ghibli/ghibli.json")
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    let html = "";
+    data.films.forEach(function (films) {
+      html += `
+            <div id="elements">
+            <img id="photos" src="${films.poster}" alt="Poster's ${films.title}">
+            <h2 id="titles"> FILM: ${films.title}<br>SCORE: ${films.rt_score}<h2>
+            </div>
     `;
+    });
+    document.getElementById("ghibli").innerHTML = html;
+  });
 
-        })
-        document.getElementById('ghibli').innerHTML = html;
-    })
-
+//  DESCRIPTION: ${films.description}, DIRECTOR: ${films.director}, PRODUCER: ${films.producer}, RELEASE DATE: ${films.release_date}
