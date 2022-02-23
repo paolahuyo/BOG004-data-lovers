d806376a678eddd48a2601388d5f62ca4da7cc2d
Paola Huyo <di.paolahuyo@gmail.com>
2022-02-23 12:53:01 -0500

    agregamos el fecth en main

--- src/data.js
+++ src/data.js
@@ -8,16 +8,23 @@ export const anotherExample = () => {
   return 'OMG';
 };
 
+export const dataFetched = fetch("data/ghibli/ghibli.json")
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (dataFetched) {
+    console.log(dataFetched);
+    let html = "";
+    dataFetched.films.forEach(function (films) {
+      html += `
+            <div id="elements">
+            <img id="photos" src="${films.poster}" alt="Poster's ${films.title}">
+            <h2 id="titles"> FILM: ${films.title}<br>SCORE: ${films.rt_score}<h2>
+            </div>
+    `;
+    });
+    document.getElementById("ghibliMovies").innerHTML = html;
+    return dataFetched;
+  });
 
-
-function searchMovie(condition){
-  const url = `data/ghibli/ghibli.json`;
-  fetch(url)
-  .then(response => response.json())
-  .then((jsonData) => {
-   console.log(jsonData.films.filter(film => film.title.includes(condition)))
-   return jsonData.films.filter(film => film.title.includes(condition)); 
- });
-}
-
-searchMovie("Totoro");
+  
--- src/main.js
+++ src/main.js
@@ -1,23 +1,17 @@
-fetch("data/ghibli/ghibli.json")
-  .then(function (res) {
-    return res.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    let html = "";
-    data.films.forEach(function (films) {
-      html += `
-            <div id="elements">
-            <img id="photos" src="${films.poster}" alt="Poster's ${films.title}">
-            <h2 id="titles"> FILM: ${films.title}<br>SCORE: ${films.rt_score}<h2>
-            </div>
-    `;
-    });
-    document.getElementById("ghibli").innerHTML = html;
-    
-  });
+import { dataFetched } from "./data.js";
+console.log(dataFetched);
 
+  function searchMovie(condition){
+    const url = `data/ghibli/ghibli.json`;
+    fetch(url)
+    .then(response => response.json())
+    .then((jsonData) => {
+     console.log(jsonData.films.filter(film => film.title.includes(condition)))
+     return jsonData.films.filter(film => film.title.includes(condition)); 
+   });
+  }
 
+  searchMovie("Totoro");
 
 
 //  DESCRIPTION: ${films.description}, DIRECTOR: ${films.director}, PRODUCER: ${films.producer}, RELEASE DATE: ${films.release_date}
