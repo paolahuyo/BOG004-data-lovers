7bb296b7d0acf3af795ae4609cebaa83387788e7
Paola Huyo <di.paolahuyo@gmail.com>
2022-02-25 12:15:53 -0500

    Anadimos 2 eventos:boton send, click en div de la pelicula

--- src/main.js
+++ src/main.js
@@ -6,7 +6,10 @@ console.log(dataFilms);
 
 const palabraClave = document.querySelector('#formulario');
 const send = document.querySelector('#btn-send');
-
+const rootMovies = document.querySelector('#rootMovies');
+const rootMovie = document.querySelector('#rootMovie');
+const titleMovie = document.querySelector('#titleMovie');
+const photoMovie = document.querySelector('#photoMovie');
 
 /*funcion para pintar los posters de las peliculas*/
 
@@ -14,7 +17,7 @@ function mostrar(data){
   let html = "";
        data.forEach(function (film) {
         html += `
-              <div id="elements">
+              <div id="elements" class="elements">
               <img id="photos" src="${film.poster}" alt="Poster's ${film.title}">
               <h2 id="titles"> FILM: ${film.title}<br>SCORE: ${film.rt_score}<h2>
               </div>
@@ -24,5 +27,23 @@ function mostrar(data){
 }
 mostrar(dataFilms)
 
-console.log (filtrarPeliculas(dataFilms, 'Castle in the Sky'))
-send.addEventListener('click',filtrarPeliculas(dataFilms,palabraClave))
+//console.log (filtrarPeliculas(dataFilms, 'Castle in the Sky'))
+const tarjetas = document.getElementsByClassName('elements');
+for ( let i=0; i< tarjetas.length; i++ ) {
+   //   console.log(tarjetas[i]);
+      tarjetas[i].addEventListener ('click', () => {
+            rootMovies.style.display= "none";
+            rootMovie.style.display= "block";
+            const movieSelected = dataFilms[i];
+            titleMovie.innerText = movieSelected.title;
+            photoMovie.setAttribute('src', 'dataFilms.poster[i]');
+
+
+      })
+}
+
+console.log(tarjetas);
+send.addEventListener('click',() => filtrarPeliculas(dataFilms,palabraClave))
+
+
+
